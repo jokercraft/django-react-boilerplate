e0658488bce010b5df979305faa8c7f2ba8a6194
unknown <serdarilarslan@gmail.com>
2020-11-12 17:38:10 +0300

    Frontend bundle settings and App-HomePage React components - updated Readme

---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <p>This is the home page</p>
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
+}
